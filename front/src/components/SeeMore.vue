8438c4a8be2adaf9403aa70cc3b4d9d7fe404bd5
davidglezz <davidg@empathy.co>
2023-09-23 18:03:31 +0200

    feat: add see more transition
    Signed-off-by: davidglezz <davidg@empathy.co>

--- front/src/components/SeeMore.vue
+++ front/src/components/SeeMore.vue
@@ -1,7 +1,27 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, type BaseTransitionProps } from 'vue';
 
 const isOpen = ref(false);
+
+const transitionHooks: BaseTransitionProps = {
+  onEnter(element) {
+    const scrollHeight = element.scrollHeight;
+    element.style.height = '0';
+    requestAnimationFrame(() => {
+      element.style.height = `${scrollHeight}px`;
+    });
+  },
+  onAfterEnter(element) {
+    element.style.height = 'auto';
+  },
+  onLeave(element) {
+    element.style.height = `${element.scrollHeight}px`;
+    requestAnimationFrame(() => (element.style.height = '0'));
+  },
+  onAfterLeave(element) {
+    element.style.removeProperty('height');
+  },
+};
 </script>
 
 <template>
@@ -14,18 +34,30 @@ const isOpen = ref(false);
       <span class="more">See more</span>
       <span class="less">See less</span>
     </div>
-    <div v-show="isOpen" class="col-md-10">
-      <div class="row">
-        <slot />
-        <div
-          class="col-md-8 push-md-1 see-more text-md-center"
-          :class="{ open: isOpen }"
-          @click="isOpen = !isOpen"
-        >
-          <span class="more">See more</span>
-          <span class="less">See less</span>
+    <transition name="slide" v-bind="transitionHooks">
+      <div v-show="isOpen" class="col-md-10">
+        <div class="row">
+          <slot />
+          <div
+            class="col-md-8 push-md-1 see-more text-md-center"
+            :class="{ open: isOpen }"
+            @click="isOpen = !isOpen"
+          >
+            <span class="more">See more</span>
+            <span class="less">See less</span>
+          </div>
         </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
+
+<style lang="scss">
+.slide {
+  &-enter-active,
+  &-leave-active {
+    transition: height 0.5s ease-in-out;
+    overflow: hidden;
+  }
+}
+</style>
