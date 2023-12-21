fe2fc75ff5363acdef51bda051b181bf058e556e
boherm <boris.hermans@prestashop.com>
2023-12-21 18:50:23 +0100

    Rebranding 2024

--- front/src/components/TopTranslators/TopTen.vue
+++ front/src/components/TopTranslators/TopTen.vue
@@ -19,22 +19,22 @@ let allCounts = computed(() =>
 <template>
   <div>
     <div class="col-md-10">
-      <h2 class="text-md-center">Top translators:</h2>
-      <div class="text-md-center date">
+      <h2 class="text-center">Top translators:</h2>
+      <div class="text-center date">
         Last update: <strong>{{ date }}</strong>
       </div>
     </div>
-    <div class="col-md-4">
+    <div class="col-xs-4">
       <Avatar
         v-for="(translator, i) in leftColumn"
         :key="`${translator.name}${translator.username}`"
         :data="translator"
       />
     </div>
-    <div class="col-md-2">
+    <div class="col-xs-2">
       <Arrow :counts="allCounts" />
     </div>
-    <div class="col-md-4 right-column">
+    <div class="col-xs-4 right-column">
       <Avatar
         v-for="(translator, i) in rightColumn"
         :key="`${translator.name}${translator.username}`"
